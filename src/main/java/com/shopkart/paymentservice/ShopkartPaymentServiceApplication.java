08449151fb97915d72f96828b8b6a31d6d3381bd
Sagar <patelsagar913@gmail.com>
2025-06-16 16:42:37 +0530

    first commit with project init

---
+++ src/main/java/com/shopkart/paymentservice/ShopkartPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shopkart.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopkartPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopkartPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopkart/paymentservice/ShopkartPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopkart.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkartPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
